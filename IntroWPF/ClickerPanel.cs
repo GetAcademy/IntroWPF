cdb6c8fee3df07a1f2e8916bcbd144deb86251f0
GET Academy <terje@kolderup.net>
2024-12-05 13:19:02 +0100

    x

---
+++ IntroWPF/ClickerPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace IntroWPF
+{
+    internal class ClickerPanel : StackPanel
+    {
+        private int _clicks;
+        private readonly Label _label;
+
+        public ClickerPanel()
+        {
+            var button = new Button { Content = "Klikk" };
+            button.Click += ButtonClick;
+            _label = new Label { Content = "0" };
+            Orientation = Orientation.Horizontal;
+            Children.Add(_label);
+            Children.Add(button);
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            _clicks++;
+            _label.Content = _clicks;
+        }
+    }
+}
--- IntroWPF/Program.cs
+++ IntroWPF/Program.cs
@@ -10,14 +10,24 @@ namespace IntroWPF
         {
             var app = new Application();
             var window = new Window();
-            var clicker1 = new Clicker();
-            var clicker2 = new Clicker();
-            var clicker3 = new Clicker();
+
+            var clicker1 = new ClickerPanel();
+            var clicker2 = new ClickerPanel();
+            var clicker3 = new ClickerPanel();
 
             var panel = new StackPanel();
-            panel.Children.Add(clicker1.Panel);
-            panel.Children.Add(clicker2.Panel);
-            panel.Children.Add(clicker3.Panel);
+            panel.Children.Add(clicker1);
+            panel.Children.Add(clicker2);
+            panel.Children.Add(clicker3);
+
+            //var clicker1 = new Clicker();
+            //var clicker2 = new Clicker();
+            //var clicker3 = new Clicker();
+
+            //var panel = new StackPanel();
+            //panel.Children.Add(clicker1.Panel);
+            //panel.Children.Add(clicker2.Panel);
+            //panel.Children.Add(clicker3.Panel);
 
             window.Content = panel;
             app.Run(window);
